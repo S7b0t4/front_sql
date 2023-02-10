7ee2bebcf9a044adbfbc6398b6f225a73dc86b16
S7b0t4 <toni.paun.00@mail.ru>
2023-02-10 22:06:47 +0500

    IDK

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [data, setData] = useState(null)
+
+  async function fetchHandler(){
+    const response = await fetch("/api");
+    console.log(response)
+    const data = response.json()
+    setData(data)
+    //const data = await response.json();
+  }
+  fetchHandler()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={data} alt=""/>
     </div>
   );
 }
